ec9f64deead2edcf7caaf34a11e2aea3fe9089ef
Ben McLaren <benmclaren@live.co.uk>
2020-11-13 17:46:23 +0000

    transitions working, images broken

--- ben_personal_site/assets/js/app.js
+++ ben_personal_site/assets/js/app.js
@@ -1,5 +1,7 @@
 
-console.log('its linked');
+console.log('app.js is linked');
+
+
 
 const hamburger = document.querySelector(".ham");
 const mainContainerTag = document.querySelector(".home-container");
@@ -9,15 +11,16 @@ hamburger.addEventListener("click", function () {
 })
 
 
-function delay (URL) {
-  setTimeout( function() { window.location = URL }, 800 );
-}
+// function delay (URL) {
+//   setTimeout( function() { window.location = URL }, 800 );
+// }
 
 const navItem = document.querySelectorAll(".nav-item");
 
 navItem.forEach((link) => {
   link.addEventListener("click", function () {
     mainContainerTag.classList.toggle("open");
+    hamburger.classList.toggle("active");
     console.log("triggers");
   })
 })
--- ben_personal_site/assets/js/swup.js
+++ ben_personal_site/assets/js/swup.js
@@ -1,4 +1,22 @@
-// import Swup from 'swup';
-const swup = new Swup();
 
-console.log("hi");
+
+function init() {
+  document.addEventListener("DOMContentLoaded", () => {
+      run();
+  });
+};
+
+
+(function() {
+  const swup = new Swup();
+})();
+
+
+// run once
+init();
+
+// this event runs for every page view after initial load
+// swup.on('contentReplaced', init);
+
+
+console.log("swup.js is linked");
